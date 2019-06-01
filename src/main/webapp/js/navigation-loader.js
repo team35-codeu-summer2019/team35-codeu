6355bc54b7680a796580fa31d600f0500dbee8d3
Li Mengyang <LIME0040@e.ntu.edu.sg>
2019-06-01 21:17:39 +0800

    Add comment

--- src/main/webapp/js/navigation-loader.js
+++ src/main/webapp/js/navigation-loader.js
@@ -34,6 +34,8 @@ function addLoginOrLogoutLinkToNavigation() {
         navigationElement.appendChild(createListItem(createLink('/user-page.html?user=' + loginStatus.username, 'Your Page')));
 
         navigationElement.appendChild(createListItem(createLink('/stats.html','Statistics')));
+
+        // more links for public feed etc. can be added here
         
         navigationElement.appendChild(
           createListItem(createLink('/logout', 'Logout')));
