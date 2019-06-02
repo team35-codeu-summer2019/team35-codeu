eb62368d64cefe976beb2cf402da51f25a35e18c
Li Mengyang <LIME0040@e.ntu.edu.sg>
2019-06-02 14:42:38 +0800

    Adding UserSelf Message Count

--- src/main/java/com/google/codeu/servlets/StatsUserServlet.java
+++ src/main/java/com/google/codeu/servlets/StatsUserServlet.java
@@ -1,20 +1,21 @@
 package com.google.codeu.servlets;
 
 import com.google.codeu.data.Datastore;
-import com.google.gson.JsonObject;
+import com.google.codeu.data.Message;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 
 /**
  * Handles fetching site statistics.
  */
-@WebServlet("/stats/map")
-public class StatsMapServlet extends HttpServlet {
+@WebServlet("/stats/user")
+public class StatsUserServlet extends HttpServlet {
 
     private Datastore datastore;
 
@@ -30,12 +31,17 @@ public class StatsMapServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
 
-        response.setContentType("application/json");
+        response.setContentType("text/html");
 
-        int messageCount = datastore.getTotalMessageCount();
+        String user = request.getParameter("user");
 
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("messageCount", messageCount);
-        response.getOutputStream().println(jsonObject.toString());
+        if (user == null || user.equals("")) {
+            // Request is invalid, return empty response
+            return;
+        }
+
+        List<Message> message = datastore.getMessages(user);
+
+        response.getOutputStream().println(message.size());
     }
 }
