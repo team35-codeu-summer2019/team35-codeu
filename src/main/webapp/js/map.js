5f8cf9142f3a1b0022373397e84a303e647db2ba
Pang Hangzhi <hangzhipang@gmail.com>
2019-06-09 07:48:12 +0800

    fixed the styling problem

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -1,48 +1,51 @@
-function createMap() {
-    addLoginOrLogoutLinkToNavigation();
-    const map = new google.maps.Map(document.getElementById('customized-map'), {
-        center: { lat: 37.422, lng: -122.084 },
-        zoom: 16
-    });
-    recenterToCurrentLocation(map);
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
 
 function recenterToCurrentLocation(map) {
-    infoWindow = new google.maps.InfoWindow;
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found!');
-            infoWindow.open(map);
-            map.setCenter(pos);
-            const trexMarker = new google.maps.Marker({
-                position: { lat: pos.lat, lng: pos.lng },
-                map: map,
-                title: 'Current Location'
-            });
-            var trexInfoWindow = new google.maps.InfoWindow({
-                content: 'Your current location.'
-            });
-            trexMarker.addListener('click', function() {
-                trexInfoWindow.open(map, trexMarker);
-            });
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
+  const infoWindow = new google.maps.InfoWindow();
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      infoWindow.setPosition(pos);
+      infoWindow.setContent("Location found!");
+      infoWindow.open(map);
+      map.setCenter(pos);
+      const trexMarker = new google.maps.Marker({
+        position: { lat: pos.lat, lng: pos.lng },
+        map,
+        title: "Current Location"
+      });
+      const trexInfoWindow = new google.maps.InfoWindow({
+        content: "Your current location."
+      });
+      trexMarker.addListener("click", () => {
+        trexInfoWindow.open(map, trexMarker);
+      });
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
+function createMap() {
+  addLoginOrLogoutLinkToNavigation();
+  const map = new google.maps.Map(document.getElementById("customized-map"), {
+    center: { lat: 37.422, lng: -122.084 },
+    zoom: 16
+  });
+  recenterToCurrentLocation(map);
 }
