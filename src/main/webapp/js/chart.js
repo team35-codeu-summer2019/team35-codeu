93c51d12a9df0b813a7f7fd8ddaa055d36c3ac19
Li Mengyang <LIME0040@e.ntu.edu.sg>
2019-06-08 13:14:47 +0800

    Added Top-10-Most-Popular-Places

---
+++ src/main/webapp/js/chart.js
@@ -0,0 +1,26 @@
+google.charts.load('current', { packages: ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var book_data = new google.visualization.DataTable();
+    book_data.addColumn('string', 'Book Title');
+    book_data.addColumn('number', 'Votes');
+
+    //add data to book_data
+    book_data.addRows([
+        ["The Best We Could Do", 6],
+        ["Sing, Unburied, Sing", 10],
+        ["The Book of Unknown Americans", 7],
+        ["The 57 Bus", 4],
+        ["The Handmaid's Tale", 8]
+    ]);
+
+    var chart = new google.visualization.BarChart(document.getElementById('top-10-places-users-been-to'));
+
+    var chart_options = {
+        width: 800,
+        height: 400
+    };
+
+    chart.draw(book_data, chart_options);
+}
