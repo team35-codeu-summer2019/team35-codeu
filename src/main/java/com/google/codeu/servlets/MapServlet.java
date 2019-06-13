471afe3deea6fce45fd7e4a9dc42ab889aa29f34
Pang Hangzhi <hangzhipang@gmail.com>
2019-06-13 20:35:16 +0800

    change java file indentation

--- src/main/java/com/google/codeu/servlets/MapServlet.java
+++ src/main/java/com/google/codeu/servlets/MapServlet.java
@@ -13,47 +13,47 @@ import java.util.Scanner;
 
 @WebServlet("/map-data")
 public class MapServlet extends HttpServlet {
-    /**
-     * Returns Map data as a JSON array, e.g. [{"lat": 38.4404675, "lng": -122.7144313}]
-     */
-    private JsonArray mapArray;
+  /**
+   * Returns Map data as a JSON array, e.g. [{"lat": 38.4404675, "lng": -122.7144313}]
+   */
+  private JsonArray mapArray;
 
-    @Override
-    public void init() {
-        mapArray = new JsonArray();
-        Gson gson = new Gson();
-        Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/country.csv"));
-        while(scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] cells = line.split(",");
+  @Override
+  public void init() {
+    mapArray = new JsonArray();
+    Gson gson = new Gson();
+    Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/country.csv"));
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      String[] cells = line.split(",");
 
-            String abbreviation = cells[0].replaceAll("[^\\x00-\\x7F]", "");
-            double lat = Double.parseDouble(cells[1].replaceAll("[^\\x00-\\x7F]", ""));
-            double lng = Double.parseDouble(cells[2].replaceAll("[^\\x00-\\x7F]", ""));
-            String countryName = cells[3].replaceAll("[^\\x00-\\x7F]", "");
+      String abbreviation = cells[0].replaceAll("[^\\x00-\\x7F]", "");
+      double lat = Double.parseDouble(cells[1].replaceAll("[^\\x00-\\x7F]", ""));
+      double lng = Double.parseDouble(cells[2].replaceAll("[^\\x00-\\x7F]", ""));
+      String countryName = cells[3].replaceAll("[^\\x00-\\x7F]", "");
 
-            mapArray.add(gson.toJsonTree(new Country(countryName,lat, lng,abbreviation)));
-        }
-        scanner.close();
+      mapArray.add(gson.toJsonTree(new Country(countryName, lat, lng, abbreviation)));
     }
+    scanner.close();
+  }
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json");
-        response.getOutputStream().println(mapArray.toString());
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+    response.getOutputStream().println(mapArray.toString());
+  }
+
+  private static class Country {
+    String countryName;
+    double lat;
+    double lng;
+    String abbreviation;
+
+    private Country(String countryName, double lat, double lng, String abbreviation) {
+      this.countryName = countryName;
+      this.lat = lat;
+      this.lng = lng;
+      this.abbreviation = abbreviation;
     }
-
-    private static class Country{
-        String countryName;
-        double lat;
-        double lng;
-        String abbreviation;
-
-        private Country(String countryName, double lat, double lng, String abbreviation) {
-            this.countryName = countryName;
-            this.lat = lat;
-            this.lng = lng;
-            this.abbreviation = abbreviation;
-        }
-    }
+  }
 }
