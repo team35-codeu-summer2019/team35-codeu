4263454537f357dde598d0e8f119f66a9a4a50b7
nanda-thant-sin <jacepot@gmail.com>
2019-06-14 19:32:54 +0630

    added quill rich text-editor and removed BBCode support library

---
+++ src/main/webapp/js/quill-editor.js
@@ -0,0 +1,18 @@
+var quill = new Quill('#editor-container', {
+  modules: {
+    toolbar: [
+      ['bold', 'italic','underline','link'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    ]
+  },
+  placeholder: 'Compose a message...',
+  theme: 'snow'
+});
+var form = document.querySelector('form');
+function copyInput() {
+  // Populate hidden form on submit
+  var message = document.querySelector('textarea[name=text]');
+  message.value = quill.root.innerHTML;
+  return true;
+};
