1a8bef2f0ac4b5b9851bef6c416c10c91a38c9a4
Dai Nguyen <nguyenddai99@gmail.com>
2019-06-15 15:40:45 +0700

    fix code style

--- src/main/java/com/google/codeu/servlets/BlobstoreUploadUrlServlet.java
+++ src/main/java/com/google/codeu/servlets/BlobstoreUploadUrlServlet.java
@@ -12,13 +12,13 @@ import java.io.IOException;
 @WebServlet("/blobstore-upload-url")
 public class BlobstoreUploadUrlServlet extends HttpServlet {
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-        String uploadUrl = blobstoreService.createUploadUrl("/form-handler");
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    String uploadUrl = blobstoreService.createUploadUrl("/form-handler");
 
-        response.setContentType("text/html");
-        response.getOutputStream().println(uploadUrl);
-    }
+    response.setContentType("text/html");
+    response.getOutputStream().println(uploadUrl);
+  }
 
 }
--- src/main/java/com/google/codeu/servlets/FormHandlerServlet.java
+++ src/main/java/com/google/codeu/servlets/FormHandlerServlet.java
@@ -31,63 +31,63 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
 
-    private Datastore datastore;
+  private Datastore datastore;
 
-    @Override
-    public void init() {
-        datastore = new Datastore();
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+  UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      response.sendRedirect("/index.html");
+      return;
+    }
+    String user = userService.getCurrentUser().getEmail();
+
+    // Get the URL of the image that the user uploaded to Blobstore.
+    String imageUrl = getUploadedFileUrl(request, "image");
+
+    String content = "<img src = \"" + imageUrl + "\">";
+
+    Message message = new Message(user, content);
+    datastore.storeMessage(message);
+
+    response.sendRedirect("/user-page.html?user=" + user);
+  }
+
+  /**
+      * Returns a URL that points to the uploaded file, or null if the user didn't upload a file.
+      */
+  private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName){
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+    List<BlobKey> blobKeys = blobs.get("image");
+
+    // User submitted form without selecting a file, so we can't get a URL. (devserver)
+    if(blobKeys == null || blobKeys.isEmpty()) {
+      return null;
     }
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Our form only contains a single file input, so get the first index.
+    BlobKey blobKey = blobKeys.get(0);
 
-        UserService userService = UserServiceFactory.getUserService();
-        if (!userService.isUserLoggedIn()) {
-            response.sendRedirect("/index.html");
-            return;
-        }
-        String user = userService.getCurrentUser().getEmail();
-
-        // Get the URL of the image that the user uploaded to Blobstore.
-        String imageUrl = getUploadedFileUrl(request, "image");
-
-        String content = "<img src = \"" + imageUrl + "\">";
-
-        Message message = new Message(user, content);
-        datastore.storeMessage(message);
-
-        response.sendRedirect("/user-page.html?user=" + user);
+    // User submitted form without selecting a file, so we can't get a URL. (live server)
+    BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+    if (blobInfo.getSize() == 0) {
+      blobstoreService.delete(blobKey);
+      return null;
     }
 
-    /**
-     * Returns a URL that points to the uploaded file, or null if the user didn't upload a file.
-     */
-    private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName){
-        BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-        Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
-        List<BlobKey> blobKeys = blobs.get("image");
+    // We could check the validity of the file here, e.g. to make sure it's an image file
+    // https://stackoverflow.com/q/10779564/873165
 
-        // User submitted form without selecting a file, so we can't get a URL. (devserver)
-        if(blobKeys == null || blobKeys.isEmpty()) {
-            return null;
-        }
-
-        // Our form only contains a single file input, so get the first index.
-        BlobKey blobKey = blobKeys.get(0);
-
-        // User submitted form without selecting a file, so we can't get a URL. (live server)
-        BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
-        if (blobInfo.getSize() == 0) {
-            blobstoreService.delete(blobKey);
-            return null;
-        }
-
-        // We could check the validity of the file here, e.g. to make sure it's an image file
-        // https://stackoverflow.com/q/10779564/873165
-
-        // Use ImagesService to get a URL that points to the uploaded file.
-        ImagesService imagesService = ImagesServiceFactory.getImagesService();
-        ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
-        return imagesService.getServingUrl(options);
-    }
+    // Use ImagesService to get a URL that points to the uploaded file.
+    ImagesService imagesService = ImagesServiceFactory.getImagesService();
+    ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
+    return imagesService.getServingUrl(options);
+  }
 }
