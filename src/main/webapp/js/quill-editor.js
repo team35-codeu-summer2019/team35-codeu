facb707a62899c4025eb5718dd7b0dfadc898b46
nanda-thant-sin <jacepot@gmail.com>
2019-06-15 09:45:35 +0630

    fix some warnings from stickler-ci

--- src/main/webapp/js/quill-editor.js
+++ src/main/webapp/js/quill-editor.js
@@ -1,19 +1,18 @@
-var quill = new Quill('#editor-container', {
+const quill = new Quill('#editor-container', {
   modules: {
     toolbar: [
-      ['bold', 'italic','underline','link'],
+      ['bold', 'italic', 'underline', 'link'],
       [{ list: 'ordered' }, { list: 'bullet' }],
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
     ]
   },
   bounds: document.querySelector('#editor-container'),
   placeholder: 'Compose a message...',
   theme: 'snow'
 });
-var form = document.querySelector('form');
 function copyInput() {
   // Populate hidden form on submit
-  var message = document.querySelector('textarea[name=text]');
+  const message = document.querySelector('textarea[name=text]');
   message.value = quill.root.innerHTML;
   return true;
-};
+}
