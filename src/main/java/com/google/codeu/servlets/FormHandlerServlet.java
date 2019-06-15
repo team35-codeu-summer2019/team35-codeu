3e0351431acef133af222d842c920869d1a33800
Dai Nguyen <nguyenddai99@gmail.com>
2019-06-15 15:45:02 +0700

    minor fix'

--- src/main/java/com/google/codeu/servlets/FormHandlerServlet.java
+++ src/main/java/com/google/codeu/servlets/FormHandlerServlet.java
@@ -41,7 +41,7 @@ public class FormHandlerServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-  UserService userService = UserServiceFactory.getUserService();
+    UserService userService = UserServiceFactory.getUserService();
     if (!userService.isUserLoggedIn()) {
       response.sendRedirect("/index.html");
       return;
