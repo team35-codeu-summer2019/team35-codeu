a9b288a3494b9cb5c6325818878cd3bf493c91a6
Pang Hangzhi <hangzhipang@gmail.com>
2019-06-21 21:34:36 +0800

    styling and indentation

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -4,20 +4,21 @@
 let map;
 let editMarker;
 const image = {
-  url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+  url:
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
   size: new google.maps.Size(20, 32),
   origin: new google.maps.Point(0, 0),
   anchor: new google.maps.Point(0, 32)
 };
 const shape = {
   coords: [1, 1, 1, 20, 18, 20, 18, 1],
-  type: 'poly'
+  type: "poly"
 };
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(
     browserHasGeolocation
-      ? 'Error: The Geolocation service failed.'
+      ? "Error: The Geolocation service failed."
       : "Error: Your browser doesn't support geolocation."
   );
   infoWindow.open(map);
@@ -26,13 +27,13 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 function recenterToCurrentLocation() {
   const infoWindow = new google.maps.InfoWindow();
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
+    navigator.geolocation.getCurrentPosition(position => {
       const pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found!');
+      infoWindow.setContent("Location found!");
       infoWindow.open(map);
       map.setCenter(pos);
       const trexMarker = new google.maps.Marker({
@@ -40,12 +41,12 @@ function recenterToCurrentLocation() {
         map,
         icon: image,
         shape,
-        title: 'Current Location'
+        title: "Current Location"
       });
       const trexInfoWindow = new google.maps.InfoWindow({
-        content: 'Your current location.'
+        content: "Your current location."
       });
-      trexMarker.addListener('click', () => {
+      trexMarker.addListener("click", () => {
         trexInfoWindow.open(map, trexMarker);
       });
     });
@@ -56,12 +57,12 @@ function recenterToCurrentLocation() {
 }
 function createMap() {
   addLoginOrLogoutLinkToNavigation();
-  map = new google.maps.Map(document.getElementById('customized-map'), {
+  map = new google.maps.Map(document.getElementById("customized-map"), {
     center: { lat: 38.5949, lng: -94.8923 },
     zoom: 4
   });
   // When the user clicks in the map, show a marker with a text box the user can edit.
-  map.addListener('click', (event) => {
+  map.addListener("click", event => {
     createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
   });
   fetchMarkers();
@@ -69,16 +70,32 @@ function createMap() {
 }
 /** Fetches markers from the backend and adds them to the map. */
 function fetchMarkers() {
-  fetch('/markers').then(response => response.json()).then((markers) => {
-    markers.forEach((marker) => {
-      createMarkerForDisplay(marker.lat, marker.lng, marker.content, image, shape);
+  fetch("/markers")
+    .then(response => response.json())
+    .then(markers => {
+      markers.forEach(marker => {
+        createMarkerForDisplay(
+          marker.lat,
+          marker.lng,
+          marker.content,
+          image,
+          shape
+        );
+      });
     });
-  });
-  fetch('/map-data').then(response => response.json()).then((markers) => {
-    markers.forEach((marker) => {
-      createMarkerForDisplay(marker.lat, marker.lng, marker.content, null, null);
+  fetch("/map-data")
+    .then(response => response.json())
+    .then(markers => {
+      markers.forEach(marker => {
+        createMarkerForDisplay(
+          marker.lat,
+          marker.lng,
+          marker.content,
+          null,
+          null
+        );
+      });
     });
-  });
 }
 function toggleBounce() {
   if (marker.getAnimation() !== null) {
@@ -103,7 +120,7 @@ function createMarkerForDisplay(lat, lng, content, icon, s) {
     const infoWindow = new google.maps.InfoWindow({
       content
     });
-    marker.addListener('click', () => {
+    marker.addListener("click", () => {
       infoWindow.open(map, marker);
       toggleBounce();
     });
@@ -117,11 +134,11 @@ function createMarkerForDisplay(lat, lng, content, icon, s) {
 /** Sends a marker to the backend for saving. */
 function postMarker(lat, lng, content) {
   const params = new URLSearchParams();
-  params.append('lat', lat);
-  params.append('lng', lng);
-  params.append('content', content);
-  fetch('/markers', {
-    method: 'POST',
+  params.append("lat", lat);
+  params.append("lng", lng);
+  params.append("content", content);
+  fetch("/markers", {
+    method: "POST",
     body: params
   });
 }
@@ -143,24 +160,24 @@ function createMarkerForEdit(lat, lng) {
     content: buildInfoWindowInput(lat, lng)
   });
   // When the user closes the editable info window, remove the marker.
-  google.maps.event.addListener(infoWindow, 'closeclick', () => {
+  google.maps.event.addListener(infoWindow, "closeclick", () => {
     editMarker.setMap(null);
   });
   infoWindow.open(map, editMarker);
 }
 /** Builds and returns HTML elements that show an editable textbox and a submit button. */
 function buildInfoWindowInput(lat, lng) {
-  const textBox = document.createElement('textarea');
-  const button = document.createElement('button');
-  button.appendChild(document.createTextNode('Submit'));
+  const textBox = document.createElement("textarea");
+  const button = document.createElement("button");
+  button.appendChild(document.createTextNode("Submit"));
   button.onclick = () => {
     postMarker(lat, lng, textBox.value);
     createMarkerForDisplay(lat, lng, textBox.value, image, shape);
     editMarker.setMap(null);
   };
-  const containerDiv = document.createElement('div');
+  const containerDiv = document.createElement("div");
   containerDiv.appendChild(textBox);
-  containerDiv.appendChild(document.createElement('br'));
+  containerDiv.appendChild(document.createElement("br"));
   containerDiv.appendChild(button);
   return containerDiv;
 }
