00b5396284ac88191ddcae9ca376f85caddcf0d8
nanda-thant-sin <jacepot@gmail.com>
2019-06-22 16:34:15 +0630

    minor fix

--- src/main/webapp/js/text-to-speech.js
+++ src/main/webapp/js/text-to-speech.js
@@ -4,18 +4,18 @@ async function play(audioId, bodyMessageId) {
   const params = new URLSearchParams();
   params.append('text', content);
   if (content === '') {
-   // Do nothing; consider showing a simple error to the user.
+    // Do nothing; consider showing a simple error to the user.
   }
   try {
     const resp = await fetch('/a11y/tts', {
-	    method: 'POST',
-	    body: params,
-	 });
-	 const audio = await resp.blob();
-	 const audioURL = URL.createObjectURL(audio);
+      method: 'POST',
+      body: params,
+   });
+   const audio = await resp.blob();
+   const audioURL = URL.createObjectURL(audio);
    const elem = document.getElementById(audioId);
-	 elem.src = audioURL;
-	 elem.play();
+   elem.src = audioURL;
+   elem.play();
   } catch (err) {
     throw new Error('Unable to call the Text to Speech API: {e}');
   }
